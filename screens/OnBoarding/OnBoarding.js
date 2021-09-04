f04a5e6e74cb75cd1460cfb6ebed6a80e69ab9b6
pavlo-osypov-by <pavlo.osypov@edvantis.com>
2021-09-04 09:57:22 +0300

    onBoardingScreen

--- screens/OnBoarding/OnBoarding.js
+++ screens/OnBoarding/OnBoarding.js
@@ -1,19 +1,136 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    ImageBackground,
+    Image,
+    Animated
 } from 'react-native';
+import { constants, images, FONTS, SIZES, COLORS } from '../../constants'
 
 const OnBoarding = () => {
+    function renderHeaderLogo() {
+        return (
+            <View
+                style={{
+                    position: 'absolute',
+                    top: SIZES.height > 800 ? 50 : 25,
+                    left: 0,
+                    right: 0,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                <Image
+                    source={images.logo_02}
+                    resizeMode='contain'
+                    style={{
+                        width: SIZES.width * 0.5,
+                        height: 100
+                    }}
+                />
+            </View>
+        )
+    }
+
+    function renderFooter() {
+        return (
+            <View
+                style={{
+                    height: 160
+                }}
+            >
+
+
+            </View>
+        )
+    }
+
     return (
         <View
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
+
+                backgroundColor: COLORS.white
             }}
         >
-            <Text>OnBoarding</Text>
+            {renderHeaderLogo()}
+
+            <Animated.FlatList
+                horizontal
+                pagingEnabled
+                data={constants.onboarding_screens}
+                scrollEventThrottle={16}
+                snapToAlignment='center'
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={(item) => `${item.id}`}
+                renderItem={({ item, index }) => {
+                    return (
+                        <View
+                            style={{
+                                width: SIZES.width
+                            }}
+                        >
+                            {/* Header  */}
+                            <View
+                                style={{
+                                    flex: 3
+                                }}
+                            >
+                                <ImageBackground
+                                    source={item.backgroundImage}
+                                    style={{
+                                        flex: 1,
+                                        alignItems: 'center',
+                                        justifyContent: 'flex-end',
+                                        height: index == 1 ? '92%' : '100%',
+                                        width: '100%'
+
+                                    }}
+                                >
+                                    <Image
+                                        source={item.bannerImage}
+                                        resizeMode='contain'
+                                        style={{
+                                            width: SIZES.width * 0.8,
+                                            height: SIZES.width * 0.8,
+                                            marginBottom: -SIZES.padding
+                                        }}
+
+                                    />
+                                </ImageBackground>
+                            </View>
+
+                            {/* Detail  */}
+                            <View
+                                style={{
+                                    flex: 1,
+                                    marginTop: 30,
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    paddingHorizontal: SIZES.radius
+                                }}
+                            >
+                                <Text style={{ ...FONTS.h1, fontSize: 25, color: COLORS.black }} >
+                                    {item.title}
+                                </Text>
+
+                                <Text
+                                    style={{
+                                        marginTop: SIZES.radius,
+                                        textAlign: 'center',
+                                        color: COLORS.darkGray,
+                                        paddingHorizontal: SIZES.padding,
+                                        ...FONTS.body
+                                    }}
+                                >
+                                    {item.description}
+                                </Text>
+                            </View>
+                        </View>
+                    )
+                }}
+            />
+            {renderFooter()}
         </View>
     )
 }
